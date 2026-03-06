383c2e558d812bb43187984bb3e953ebb2a14dd5
sist-6 <sist-6@sist6>
2026-03-06 11:46:04 +0900

    11111

---
+++ Chapter04_제어문/src/제어문_선택조건문.java
@@ -0,0 +1,19 @@
+/*
+ * 	선택조건문
+ * 	=> 프로그램에서는 가장 많이 사용되는 문장
+ * 
+ * 	else문장은 바로 위에 있는 if문장만 제어
+ * 
+ * 	=> else문장은 단독으로 사용할 수 없다 => if문을 동반
+ *	
+ * 
+ * 
+ */
+public class 제어문_선택조건문 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
