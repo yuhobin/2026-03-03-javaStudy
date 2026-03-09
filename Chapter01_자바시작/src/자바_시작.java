5dc589f37fd9b9c60fd8538861cd789b037dd61c
sist-6 <sist-6@sist6>
2026-03-09 17:10:49 +0900

    11111

---
+++ Chapter01_자바시작/src/자바_시작.java
@@ -0,0 +1,32 @@
+/*
+ * 	자바 => 모든 소스는 클래스 안에 첨부
+ * 		=> JVM이 가장 먼저 실행하는 부분 => main (진입점)
+ * 	이미 만들어진 클래스 읽기 
+ * 	-----------------개발자 / Java 제공
+ * 	import
+ * 	=> 미국식 => 변경해서 사용
+ * 		------------------오버라이딩
+ * 	package
+ * 	import
+ * 	public class ClassName
+ * 		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		}
+		static public void main(String[] args) {
+			// TODO Auto-generated method stub
+		}
+		public static void main(String[] a) {
+			// TODO Auto-generated method stub
+		} 
+ * 
+ * 
+ * 
+ * */
+public class 자바_시작 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+	}
+
+}
